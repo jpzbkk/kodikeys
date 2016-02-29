0077e848ca0c6f673f550610c5233b900ef88173
Eric Bartels <ebartels@gmail.com>
2016-02-28 17:31:46 -0800

    Use es6 promises to handle exit & errors further up the chain
    This means our lib modules are not handling exiting the process, or
    terminating on error themselves.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var fs = require('fs');
 var path = require('path');
 var xec = require('xbmc-event-client');
@@ -7,34 +9,43 @@ var log = require('./lib/logging');
 
 var kodikeys = function(opt) {
 
-  var defaults = {
+  opt = Object.assign({}, {
     port: 9777,
     log_level: 'warn',
-  }
-  opt = Object.assign({}, defaults, opt);
+  }, opt);
 
   log.setLevel(opt.log_level)
 
-  var kodi = new xec.XBMCEventClient('kodikeys', {
-    host: opt.host,
-    port: opt.port,
+  return new Promise( (resolve, reject) => {
+
+    var kodi = new xec.XBMCEventClient('kodikeys', {
+      host: opt.host,
+      port: opt.port,
+    })
+
+    kodi.connect(function(errors, bytes) {
+      if (errors.length) {
+        let msg = `Connection failed to host ${opt.host}, port ${opt.port}`;
+        log.error(msg)
+        log.error(errors[0].toString());
+        reject(msg, errors);
+        return;
+      }
+
+      // init keyboard interactivity
+      keyboard.init(kodi)
+        .then(function finished() {
+          resolve();
+        });
+
+      // ping to keep connection  alive
+      setInterval(kodi.ping.bind(kodi), 55 * 1000);
+
+      console.log(`connected to ${opt.host} port ${opt.port}`);
+    });
+
   })
 
-  kodi.connect(function(errors, bytes) {
-    if (errors.length) {
-      log.error(`Connection failed to host ${opt.host}, port ${opt.port}`)
-      log.error(errors[0].toString());
-      process.exit(1);
-    }
-
-    // init keyboard interactivity
-    keyboard.init(kodi);
-
-    // ping to keep connection  alive
-    setInterval(kodi.ping.bind(kodi), 55 * 1000);
-
-    console.log(`connected to ${opt.host} port ${opt.port}`);
-  });
 }
 
 module.exports = kodikeys;
